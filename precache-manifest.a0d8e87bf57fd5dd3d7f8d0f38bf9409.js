d40c8c288c9eee0f5e57dce76f68031503e93df1
sumeetrana2000 <sumeetrana2000@gmail.com>
2022-11-19 22:02:18 +0530

    Updates

---
+++ precache-manifest.a0d8e87bf57fd5dd3d7f8d0f38bf9409.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3598d264f116657922f5",
+    "url": "/blackngreen/static/js/main.3598d264.chunk.js"
+  },
+  {
+    "revision": "c39cbbeef9e8130d419f",
+    "url": "/blackngreen/static/js/1.c39cbbee.chunk.js"
+  },
+  {
+    "revision": "4da201f0f7bf5711ca0d",
+    "url": "/blackngreen/static/js/runtime~main.4da201f0.js"
+  },
+  {
+    "revision": "561588d0d57fb6159ca0a3784f7f6006",
+    "url": "/blackngreen/index.html"
+  }
+];
